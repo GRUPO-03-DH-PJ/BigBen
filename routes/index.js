d292a8e964810d72f464185834d816c195859e8c
GustavoDuqueLeo <gustavodleo22@gmail.com>
2023-03-03 21:35:05 -0300

    Configurando-Projeto-Node

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('produtosDestaque');
 });
+
+router.get('/produtos/detalhes', function(req, res, next) {
+  res.render('detalheDeProduto');
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('');
+});
+
 
 module.exports = router;
