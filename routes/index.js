d61710b87aa52ee3e6bf19a489b6642556ca02ee
José Elias Miguel Seraidarian <eliasjems@gmail.com>
2023-03-07 18:59:50 -0300

    Ajustes ejs e rotas

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,19 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('produtosDestaque');
+    res.render('produtosDestaque');
 });
 
 router.get('/produtos/detalhes', function(req, res, next) {
-  res.render('detalheDeProduto');
+    res.render('detalheDeProduto');
 });
 
 router.get('/home', function(req, res, next) {
-  res.render('');
+    res.render('');
+});
+
+router.get('/categorias', function(req, res, next) {
+    res.render('paginaCategorias');
 });
 
 
