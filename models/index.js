07f748c12dbe98eec0e28647f771cecb8fd5337a
José Elias Miguel Seraidarian <eliasjems@gmail.com>
2023-04-16 13:53:31 -0300

    reposicionamento

--- models/index.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('bigben', 'root', 'dh_grupo3_pi', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
-
-module.exports = sequelize;
