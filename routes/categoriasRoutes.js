e5734d7fdf81e1839efbdd68900c6e6027046704
José Elias Miguel Seraidarian <eliasjems@gmail.com>
2023-04-18 22:53:38 -0300

    ajustes nomenclatura

--- routes/categoriasRoutes.js
+++ routes/categoriasRoutes.js
@@ -1,15 +1,9 @@
 const express = require('express');
-const router = express.Router(); <<
-<< << < HEAD
-const categoriasController = require('../controllers/categoriasController'); ===
-=== =
-const categoriasController = require('../controllers/categoriasController'); >>>
->>> > ebb132ecf6dde931c7383589b656d811ddb1f5f0
+const router = express.Router();
+const categoriasController = require('../controllers/categoriasController');
 
 router.post('/', categoriasController.create);
 router.get('/', categoriasController.findAll);
 router.get('/:id', categoriasController.findOne);
 router.put('/:id', categoriasController.update);
 router.delete('/:id', categoriasController.delete);
-
-module.exports = router;
