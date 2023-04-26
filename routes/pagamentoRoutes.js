a857e7cf0200759fdcb9504851a3e0fa64327873
José Elias Miguel Seraidarian <eliasjems@gmail.com>
2023-04-26 10:13:43 -0300

    inserindo daods do cliente na DB

--- routes/pagamentoRoutes.js
+++ routes/pagamentoRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const PagamentoController = require('../controllers/PagamentoController');
 
-router.post('/pagamentos', PagamentoController.create);
-router.get('/pagamentos', PagamentoController.findAll);
-router.get('/pagamentos/:id', PagamentoController.findOne);
-router.put('/pagamentos/:id', PagamentoController.update);
-router.delete('/pagamentos/:id', PagamentoController.delete);
+router.post('/', PagamentoController.create);
+router.get('/', PagamentoController.findAll);
+router.get('/:id', PagamentoController.findOne);
+router.put('/:id', PagamentoController.update);
+router.delete('/:id', PagamentoController.delete);
 
 module.exports = router;
