bf172cb6392b1d30aa617f154032ab1d10863dc7
GustavoDuqueLeo <gustavodleo22@gmail.com>
2023-05-15 19:39:14 -0300

    add-session-cookies-login

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,12 +1,23 @@
-const {
-  where
-} = require('sequelize');
-const User = require('../models/cliente')
+const { where } = require('sequelize');
+const User = require('../models/cliente');
 const bcrypt = require('bcrypt');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+
+// Inicializa os middlewares de sessão e cookie parser
+app.use(cookieParser());
+app.use(
+  session({
+    secret: 'sua-chave-secreta',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true } // Defina a opção secure do cookie com base na sua configuração
+  })
+);
 
 function login(req, res) {
-  return res.render('loginForm')
-};
+  return res.render('loginForm');
+}
 
 async function loggingIn(req, res) {
   let userToLogin = await User.findOne({
@@ -14,11 +25,13 @@ async function loggingIn(req, res) {
       EmailCliente: req.body.email
     }
   });
-  console.log(userToLogin)
+  console.log(userToLogin);
   if (userToLogin) {
-    let isPasswordVerified = bcrypt.compareSync(req.body.Senha, userToLogin.Senha)
+    let isPasswordVerified = bcrypt.compareSync(req.body.Senha, userToLogin.Senha);
     if (isPasswordVerified) {
-      return res.render('painelUsuario')
+      // Define a sessão do usuário
+      req.session.user = userToLogin;
+      return res.render('painelUsuario');
     }
 
     return res.render('loginForm', {
@@ -37,12 +50,9 @@ async function loggingIn(req, res) {
       }
     }
   });
-};
-
-
-
+}
 
 module.exports = {
   login,
   loggingIn,
-}
+};
