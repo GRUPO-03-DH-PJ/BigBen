d2147633f1ad726fac7ef56123e71e10af16a811
Geovanni Santos <117683781+Geovannydynoo@users.noreply.github.com>
2023-05-16 19:39:25 -0300

    pull do controllers

--- controllers/painelController.js
+++ controllers/painelController.js
@@ -1,8 +1,12 @@
-const painelController = {
-    painelUsuario: (req, res, next) => {
-        return res.render('painelUsuario');
-    }
-}
+const Cliente = require('../models/cliente');
 
+const painelController = {
+  painelUsuario: async (req, res) => {
+    const cliente = await Cliente.findByPk(req.params.id)
+    return res.render('painelUsuario', {
+      cliente
+    });
+  },
+}
 
 module.exports = painelController;
--- routes/painelRoutes.js
+++ routes/painelRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const painelController = require('../controllers/painelController');
 
-router.get('/', painelController.painelUsuario)
+router.get('/:id', painelController.painelUsuario)
 
 module.exports = router;
